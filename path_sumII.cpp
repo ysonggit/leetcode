253ea6f68986cb3b2a1fdf2af9ee3336b06a9239
ysonggit <song24@email.sc.edu>
2014-12-10 23:23:27 -0500

    simple test case for pathsumII

--- path_sumII.cpp
+++ path_sumII.cpp
@@ -18,7 +18,7 @@ int getSum(vector<int> v){
     }
     return sum;
 }
-// trick : parameter path should not be passed by reference
+
 void dfs(vector<vector<int> > & allpaths, TreeNode * root, vector<int> path, int sum){
     path.push_back(root->val);
 
@@ -46,26 +46,26 @@ vector<vector<int> > pathSum(TreeNode *root, int sum) {
 }
 
 int main(){
-    int sum = 1;
-    TreeNode * n1 = new TreeNode(0);
-    TreeNode * n2 = new TreeNode(1);
-    TreeNode * n3 = new TreeNode(1);
-    // TreeNode * n4 = new TreeNode(11);
-    // TreeNode * n5 = new TreeNode(13);
-    // TreeNode * n6 = new TreeNode(4);
-    // TreeNode * n7 = new TreeNode(7);
-    // TreeNode * n8 = new TreeNode(2);
-    // TreeNode * n9 = new TreeNode(5);
-    // TreeNode * n10 = new TreeNode(1);
+    int sum = 22;
+    TreeNode * n1 = new TreeNode(5);
+    TreeNode * n2 = new TreeNode(4);
+    TreeNode * n3 = new TreeNode(8);
+    TreeNode * n4 = new TreeNode(11);
+    TreeNode * n5 = new TreeNode(13);
+    TreeNode * n6 = new TreeNode(4);
+    TreeNode * n7 = new TreeNode(7);
+    TreeNode * n8 = new TreeNode(2);
+    TreeNode * n9 = new TreeNode(5);
+    TreeNode * n10 = new TreeNode(1);
     n1->left = n2;
     n1->right = n3;
-    //n2->left = n4;
-    // n4->left = n7;
-    // n4->right = n8;
-    // n3->left = n5;
-    // n3->right = n6;
-    // n6->left = n9;
-    // n6->right = n10;
+    n2->left = n4;
+    n4->left = n7;
+    n4->right = n8;
+    n3->left = n5;
+    n3->right = n6;
+    n6->left = n9;
+    n6->right = n10;
     vector<vector<int> > allpaths;
     allpaths = pathSum(n1, sum);
     print2DVector(allpaths);
