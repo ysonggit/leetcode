0747f07d0152f1bfa7119e24f2e351ca51be5b20
ysonggit <ysong.sc@gmail.com>
2014-12-28 00:15:17 -0500

    Optimize solution for find minimum in rotated sorted array II, 52ms

--- find_min_in_rotated_sorted_arrayII.cpp
+++ find_min_in_rotated_sorted_arrayII.cpp
@@ -7,15 +7,6 @@ using namespace std;
      then mini element locates in between
   if two elements in two halves are equal, search two sides in order
  */
-int searchInOrder(vector<int> & num, int front, int back, int cur_min){
-    int mini = cur_min;
-    for(int i= front; i<=back; i++){
-        if(num[i] < mini){
-            mini = num[i];
-        }
-    }
-    return mini;
-}
 
 // question: if null array, return ?
 int findMin(vector<int> &num) {
@@ -23,25 +14,21 @@ int findMin(vector<int> &num) {
     int front = 0;
     int back = num.size() - 1;
     int cur_min = num[0];
-    while(front <= back){
+    while(front < back){
         int mid = (front + back)/2;
-        if(num[mid]== num[front] && num[mid]==num[back]){
-            // search in order
-            return searchInOrder(num, front, back, cur_min);
-        }
         if(num[mid] > num[back]){
             // min is in the right half
-            cur_min = min(num[back], cur_min);
+            cur_min = min(cur_min, num[back]);
             front = mid+1;
-            continue;
         }
-        if(num[mid] <= num[back]){
+        else if(num[mid] < num[back]){
             cur_min = min(cur_min, num[mid]);
-            back = mid;
-            continue;
+            back = mid-1;
+        }else{
+            back--;
         }
     }
-    return cur_min;
+    return min(cur_min, min(num[front], num[back]));
 }
 
 TEST(FindMin, One){
