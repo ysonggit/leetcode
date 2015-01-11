43f81667d3582d849e60742d46e88b028f5ec363
ysonggit <ysong.sc@gmail.com>
2015-01-10 22:53:42 -0500

    AC solution permutation sequence 10ms

--- permutation_sequence.cpp
+++ permutation_sequence.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-/*
+/* time exceeds limit
   dfs template for permutations
   @param & results
   @param n
@@ -22,6 +22,7 @@ using namespace std;
          used[i] = false
          sol.pop_back
  */
+/*
 void dfs(vector<string> & results, vector<int> & sol, int n, int k, int & count, bool *used){
     if(count>k) return;
     if(sol.size()==n){
@@ -46,7 +47,7 @@ void dfs(vector<string> & results, vector<int> & sol, int n, int k, int & count,
     }
 }
 
-string getPermutation(int n, int k) {
+string getPermutation2(int n, int k) {
     if(n<=0 || k<1 ) return string(""); // k > n! overflow
     int count = 0;
     vector<string> results;
@@ -60,9 +61,48 @@ string getPermutation(int n, int k) {
     used = NULL;
     return results[0];
 }
+*/
+
+/*
+  let the number sequence of k^th permutation be a1, a2, ... an 
+  k1 = k
+  a1 = k1 / (n-1)!
+  k2 = k1 % (n-1)!
+  a2 = k2 / (n-2)!
+  .
+  .
+  .
+  kn-1 = kn-2 % 2!
+  an-1 = kn-1 / 1!
+  an = 0
+ */
+// n is less than 10 so no overflow
+int factorial(int n){
+    if(n==1 || n==0) return 1;
+    return factorial(n-1) * n;
+}
+
+string getPermutation(int n, int k) {
+    string perm;
+    if(n<1 || n>9 || k<1) return perm;
+    string numbers("123456789");
+    int a=0;
+    int total = factorial(n);
+    k--; //tricky: index is zero-based 
+    for(int i=0; i<n; i++){
+        total /= n-i;
+        a =  k/ total;
+        perm.push_back(numbers[a]);
+        //tricky: DO NOT FORGET to Remove this character since  it has been used
+        numbers.erase(numbers.begin()+a);
+        k = k% total;
+    }
+    return perm;
+}
 
 int main(int argc, char *argv[])
 {
+    cout<<getPermutation(3, 4)<<endl;
     cout<<getPermutation(9, 155915)<<endl;
     return 0;
 }
