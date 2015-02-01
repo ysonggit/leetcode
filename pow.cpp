69d0c8bea0866ba596b789a2e61037145373f2de
ysonggit <ysong.sc@gmail.com>
2015-02-01 15:54:32 -0500

    AC solutions pow & sqrt (divide and conquer )

---
+++ pow.cpp
@@ -0,0 +1,29 @@
+#include "utils.h"
+
+/*
+  divide and conquer
+  pow(x,n) = pow(x, n/2) * pow(x, n/2) * x^{n%2}
+
+  n is negative
+  1/pow(x, abs(n))
+*/
+double pow(double x, int n){
+    if(n==0) return 1;
+    if(n==1) return x;
+    double v = pow(x, abs(n)>>1);
+    double vv = v * v;
+    if(n&1==1){
+        vv *= x;
+    }
+    return (n<0) ? 1/vv : vv;
+}
+
+int main(int argc, char *argv[]){
+    for(int i=0; i<10; i++){
+        std::cout<<pow(2.0, i)<<std::endl;
+        std::cout<<pow(2.0, -i)<<std::endl;
+    }
+    return 0;
+}
+
+
--- sqrt.cpp
+++ sqrt.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
+#include "utils.h"
+
 using namespace std;
 
 /*
-   Newton's method
-   
- */
+   Newton's method   
 
 double absval(double a){
     return (a<0)? -a : a;    
@@ -17,9 +16,39 @@ int sqrt(int x){
         guess = 0.5*(x/guess+guess);
     }
     return (int)guess;
+    }*/
+/*
+  note that the returned type is int
+  means using divide and conquer can be more efficient 
+
+  x < 0? return ?
+  overflow?
+*/
+int sqrt(int x){
+    if(x<0) return -1;
+    if(x==0) return 0;
+    int left = 1, right = x/2+1;
+    while(left <= right){
+        int mid = (int)(left+right)/2;
+        // do not use mid * mid == x due to overflow
+        // check if x/mid satisfy both lower and upper bound [mid, mid+1)
+        if(x/mid >= mid && x/(mid+1) < mid+1){
+            return mid;
+        }
+        if(x/mid < mid){
+            // mid is greater 
+            right = mid-1;
+        }else if(x/mid > mid){
+            left = mid +1;
+        }
+    }
+    return 1;
 }
 
 int main(){
-    cout<<sqrt(4)<<endl;
+    for(int i=0; i<10; i++){
+        cout<<sqrt(i)<<endl;
+    }
+    cout<<sqrt(INT_MAX)<<endl;
     return 0;
 }
