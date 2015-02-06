6bc2dbd3280d34374f8f9a1c3c7193131e5c8719
ysonggit <ysong.sc@gmail.com>
2015-02-05 23:59:51 -0500

    add test case same tree

--- same_tree.cpp
+++ same_tree.cpp
@@ -15,6 +15,23 @@ TEST(SameTree, I){
     ASSERT_EQ(true, isSameTree(p,q));
 }
 
+TEST(SameTree, II){
+    string vals = "{4,2,5,1,3,#,6}";
+vector<TreeNode*> p, q;
+    treeInitializer(p, vals);
+treeInitializer(q, vals);
+ASSERT_EQ(true, isSameTree(p[0],q[0]));
+}
+
+TEST(SameTree, III){
+    string vals = "{4,2,5,1,3,#,6}";
+vector<TreeNode*> p, q;
+    treeInitializer(p, vals);
+string vals1 = "{4,2,5,3,1,#,6}";
+treeInitializer(q, vals1);
+ASSERT_EQ(false, isSameTree(p[0],q[0]));
+}
+
 int main(int argc, char *argv[]){
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
