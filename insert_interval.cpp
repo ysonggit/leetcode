5c36b4730cc371755aeed77033ab0e620c48bf6c
yang <ysong.sc@gmail.com>
2015-02-14 15:58:32 -0500

    AC solution insert interval 20ms

--- insert_interval.cpp
+++ insert_interval.cpp
@@ -1,47 +1,68 @@
 #include "utils.h"
 
 using namespace std;
-// assume it1 has overlap with it2
-Interval mergeIntervals(Interval & it1, Interval & it2){
-    int _start = min(it1.start, it2.start);
-    int _end = max(it1.end, it2.end);
-    return Interval(_start, _end);
-}
+
+/*
+  example:
+  Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+  idea: O(n^2) time O(1) space
+  starting from the first interval, find the position to insert given interval
+  if no interval or all intervals end before newInterval.start, insert to the end of the list
+  for the interval i,
+  1 if newInterval.end < i.start
+      insert newInterval here
+  2 if newInterval.start  <= i.end 
+     1) extend newInterval by  merging it with i
+     2) erase original interval i
+  Time Limit Exceeded
 
 vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-    vector<Interval> result;
-    // 1. find the position to insert the new interval
-    int i=0;
-    while( i< intervals.size()){
-        Interval it = intervals[i];
-        if( it.start <= newInterval.start && it.end>=  newInterval.end){
+    vector<Interval>::iterator iter = intervals.begin();
+    while(iter != intervals.end()){
+        if(newInterval.end < iter->start){
+            intervals.insert(iter, newInterval);
             return intervals;
-        }
-        if(it.end< newInterval.start){
-            result.push_back(it);
+        }else if(newInterval.start > iter->end){
+            iter++;
+            continue;
         }else{
-            break;
+            newInterval.start = min(newInterval.start, iter->start);
+            newInterval.end = max(newInterval.end, iter->end);
+            intervals.erase(iter); // erase takes linear time 
         }
-        i++;
     }
-    // 2. insert the new interval
-    result.push_back(newInterval);
-    // 3. check overlap of the new interval and following intervals
-    while(i<intervals.size()){
-        Interval cur_it = intervals.at(i);
-        if(result.back().end >= cur_it.start ){
-            result.back() = mergeIntervals(result.back(), cur_it);
-            i++;
+    // for case the new interval should be the last one in the list
+    intervals.insert(intervals.end(), newInterval);
+    
+    return intervals;
+    }
+*/
+/*
+  Optimiazed version: O(n) time , O(n) space
+  20 ms
+ */
+
+vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+    vector<Interval> res;
+    auto iter = intervals.begin();
+    bool inserted = false;
+    for( ; iter != intervals.end(); ++iter){
+        if(newInterval.end< iter->start){
+            if(!inserted){
+                inserted = true;
+                res.push_back(newInterval);
+            }
+            res.push_back(*iter);
+        }
+        else if(newInterval.start > iter->end){
+            res.push_back(*iter);
         }else{
-            break;
+            newInterval.start = min(newInterval.start, iter->start);
+            newInterval.end = max(newInterval.end, iter->end);
         }
     }
-    // 4. append the rest intervals (if any)
-    while(i<intervals.size()){
-        result.push_back(intervals.at(i));
-        i++;
-    }
-    return result;
+    if(!inserted) res.push_back(newInterval);
+    return res;
 }
 
 int main(int argc, char *argv[]){
