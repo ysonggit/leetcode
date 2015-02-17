eca1957fac02cce8e508d1cf4ee17bf3d4485863
yang <ysong.sc@gmail.com>
2015-02-17 02:21:29 -0500

    AC solution word search 28ms

--- word_search.cpp
+++ word_search.cpp
@@ -4,37 +4,41 @@ using namespace std;
 /*
   idea: do dfs starting from every single letter in the grid
         trick: use a 2d matrix to record visited position
+        if current char in board is not equal to the char in the target word
+        or if current char in board is already visited, then return false
  */
-// r and c are row and column indices of the starting letter
+// i and j are row and column indices of the starting letter
 // cur_idx is the index of current letter in given word
-bool dfs(const vector<vector<char> > &board, const string& word, int r, int c, int cur_idx, vector<vector<int> > & visited){
-    if(r<0 || r>=board.size() || c<0 || c>=board[0].size() ) return false;
-    char cur = word[cur_idx];
-    if(cur != board[r][c] || visited[r][c] == 1 )  return false;
-    if(visited[r][c] == 0 && cur == board[r][c] && cur_idx ==word.length()-1){
-        return true;
-    }
-
+bool dfs(const vector<vector<char> > &board,
+    const string& word,
+    vector<vector<int> > & visited,
+    int i, int j,
+    int cur_idx)
+{
+    if(i<0 || i>=board.size() || j>= board[0].size() || j<0 ) return false;
+    if(visited[i][j] != 0) return false;
+    if(board[i][j] != word[cur_idx] ) return false;
+    if(cur_idx == word.length()-1 && word[cur_idx]==board[i][j]) return true;
     bool res = false;
-    if(cur == board[r][c]){
-        visited[r][c] = 1;
-        res = dfs(board, word, r+1, c, cur_idx+1, visited) ||
-            dfs(board, word, r-1, c, cur_idx+1, visited) ||
-            dfs(board, word, r, c+1, cur_idx+1, visited) ||
-            dfs(board, word, r, c-1, cur_idx+1, visited);
-        visited[r][c] = 0;
-    }
+    visited[i][j] = 1;
+    // search four directions
+    res = dfs(board, word, visited,  i+1, j, cur_idx+1)
+        || dfs(board, word, visited, i, j+1, cur_idx+1)
+        || dfs(board, word, visited, i-1, j, cur_idx+1)
+        || dfs(board, word, visited, i, j-1, cur_idx+1);
+    visited[i][j] = 0; // donot forget this!
+
     return res;
 }
 
 bool exist(vector<vector<char> > &board, string word) {
-    if(board.size()==0) return false;
     int rows = board.size();
+    if(rows==0) return word.empty();
     int cols = board[0].size();
     vector<vector<int> > visited(rows, vector<int>(cols, 0));
-    for(int i=0; i<rows; i++){
-        for(int j=0; j<cols; j++){
-            if(dfs(board, word, i, j, 0, visited)){
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if(dfs(board, word, visited,  i, j, 0)) {
                 return true;
             }
         }
@@ -42,22 +46,29 @@ bool exist(vector<vector<char> > &board, string word) {
     return false;
 }
 
-int main(int argc, char *argv[]){
-    // testing::InitGoogleTest(&argc, argv);
-    vector<vector<char> > board{
+vector<vector<char> > board{
         {'A','B','C','E'},
         {'S','F','C','S'},
         {'A','D','E','E'}
-    };
-    //print2DVector(board);
-    string words[] ={"ABCCED", "SEE", "ABCB"};
-    for(int i=0; i<3; i++){
-        if(exist(board, words[i])){
-            cout<<"Exist!"<<endl;
-        }else{
-            cout<<"Does not exist."<<endl;
-        }
-    }
-    return 0;
+};
+
+TEST(WordSearch, I){
+    string word("ABCCED");
+    ASSERT_EQ(true, exist(board, word));
+}
+
+TEST(WordSearch, II){
+    string word("SEE");
+    ASSERT_EQ(true, exist(board, word));
+}
+
+TEST(WordSearch, III){
+    string word("ABCB");
+    ASSERT_EQ(false, exist(board, word));
+}
+
+int main(int argc, char *argv[]){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
