262115aefff1fd1f666f12e93dc72349816ded12
yang <ysong.sc@gmail.com>
2015-03-04 18:22:55 -0500

    smart solution for largest rectangle in histogram 37ms

--- largest_rectangle_in_histogram.cpp
+++ largest_rectangle_in_histogram.cpp
@@ -14,66 +14,60 @@ using namespace std;
       push i
 
       http://blog.csdn.net/linhuanmars/article/details/20524507
+
+6|      _
+5|    _| |
+4|   | | |
+3|   | | |  _
+2|_  | | |_| |
+1| |_| | | | |
+0|_|_|_|_|_|_|___
+key: how to trace the max_area composed by current bar and popped bars?
+
+max_area = max(max_area, 
+               popped_height * cur_idx-top_idx_left_instack -1 || 
+               popped_height * cur_idx_if_stack_is_empty) // when all saved heights are popped out
+max_area = 0
+
+i=0 stack push i
+    stack: 0, 
+i=1 A[1] < A[stack.top]
+    pop, max_area = max(0,  A[0]*1) = 2
+    push i to stack,
+    stack: 1 
+i=2 A[2]=5, push i 
+    stack: 2,1 
+i=3 A[3]=6, push i 
+    stack: 3,2,1 
+i=4 A[4]=2, 
+    pop  max_area = max(2,  A[3]*(4-2-1)) = 6
+    stack: 2,1
+    pop  max_area = max(6, A[2]*(4-1-1)) = 10
+    stack: 1    
+i=5 A[5]=3, push i
+    stack: 3,1
+    
+    calculate the area remaining in the stack
 */
-/*
 int largestRectangleArea(vector<int> &height) {
-    if(height.size()==0) return 0;
-    stack<int> s;
-    int n= height.size();
-    int max_rect = 0; 
-    int cur_height = 0;
-    int max_area_popped = 0;
-    for(int i=0; i< n; i++){
-        cur_height =  height[i];
-        while(!s.empty() && cur_height <= height[s.top()] ){
-            int cur_idx = s.top();
-            s.pop();
-            int max_area_popped = s.empty() ? i * height[cur_idx] :  (i-s.top()-1)*height[cur_idx];
-            max_rect = max(max_rect, max_area_popped);
+    if(height.empty()) return 0;
+    int max_area = 0;
+    stack<int> highest_bars_left;
+    height.push_back(0); // make sure finally the stack will be empty
+    int n = height.size();
+    for(int idx=0; idx<n; idx++){
+        while(!highest_bars_left.empty() && 
+            height[idx] < height[highest_bars_left.top()]){
+            int highest_idx = highest_bars_left.top();
+            highest_bars_left.pop();
+            // update max_area
+            int width = highest_bars_left.empty() ? idx : (idx-highest_bars_left.top()-1);
+            max_area = max(max_area, width * height[highest_idx]);
         }
-        s.push(i);
+        highest_bars_left.push(idx);
     }
-    // so far, a special case is not considered think of 2,1,2 or 1, 1
-    // the first bar actually contributes to the max rectangle with area 3
-    while(!s.empty() ){
-        int cur_idx = s.top();
-        s.pop();
-        int cur_area = s.empty() ? n* height[cur_idx] :  (n-s.top()-1)*height[cur_idx];
-        max_rect = max(max_rect, cur_area);
-     }
-       
-    return max_rect; 
-    }*/
-
-/*
-  idea:
-  use two stacks: one saves values, one saves indices
-  6|      __
-  5|    _| |
-  4|   | | |
-  3|   | | |  __
-  2|__ | | |_| |
-  1| |_| |_| | |
-  0|_|_|_|_|_|_|___
-
-  i=0 valuestack push 2,             valuestack: 2,    idxstack:
-
-  i=1 A[1] < valuestack.peek, pop 2, max_area = 2,
-      push A[i] to valuestack,
-      push popped idx 0 to idxstack  valuestack: 1,    idxstack: 0
-
-  i=2 A[2]=5, push 5,               valuestack: 5, 1   idxstack: 0
-
-  i=3 A[3]=6, push 6                valuestack: 6,5,1  idxstack: 0
-
-  i=4 A[4]=2, pop 6  max_area = max(max_area, 3*1, 6*1, 5*2) =10
-              pop 5 max_area = 10
-              push last popped idx 2 to idxstack
-              push 2 to value stack  valuestack: 2,1   idxstack 2,0
-
-  i=5 A[5]=3, push 3 to valuestack valuestack: 3,2,1, idxstack 2,0
-  
- */
+    return max_area;
+}
 
 TEST(LargestRectangle, One){
     vector<int> height = {2,1,5,6,2,3};
@@ -100,7 +94,7 @@ TEST(LargestRectangle, Four){
 }
 
 TEST(LargestRectangle, Five){
-    vector<int> height = {2,4};
+    vector<int> height = {4,2};
     int res = 4;
     ASSERT_EQ(res, largestRectangleArea(height));
 }
