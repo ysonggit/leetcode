ee2b08839d8fd6998bbe668f91de99f97689f7a7
yang <ysong.sc@gmail.com>
2015-03-13 01:05:49 -0400

    AC solution majority element Moore voting algorithm 24ms

--- majority_element.cpp
+++ majority_element.cpp
@@ -2,46 +2,40 @@
 #include <gtest/gtest.h>
 using namespace std;
 
-/* Basic idea: use a hash map, O(n) time and O(n) extra space
-   Optimized version: moore's voting algorithm O(n) time constant extra space
-   http://www.geeksforgeeks.org/majority-element/
-   The idea is that, since the majority element shows more than n/2 times
-   so cancel it out with other elements, finally it will be kept
+/*
+  Basic idea: use a hash map, O(n) time and O(n) extra space
+  Optimized version: moore's voting algorithm O(n) time constant extra space
+  http://www.geeksforgeeks.org/majority-element/
+  The idea is that, since the majority element shows more than n/2 times
+  so cancel it out with other elements, finally it will be kept 
+ Moore's voting algorithm
+ If the counter is 0, we set the current candidate to x and the counter to 1.
+ If the counter is not 0, we increment or decrement the counter based on whether x is the current candidate.
+*/
 
-   initialize idx and count of the majority element
-   maj_idx = 0, count = 1
-
-   for each i = 1 ... n-1
-       if A[maj_idx] == A[i]
-           count++
-       else
-           count--
-       if count == 0
-          maj_idx = i
-          count = 1
-   return A[maj_idx]
- */
-
-int majorityElement(vector<int> &num){
-    int maj_idx = 0;
-    int count = 1;
-    for(int i=1; i<num.size(); i++){
-        if(num[maj_idx]==num[i]){
-            count++;
-        }else{
-            count--;
-        }
-        if(count==0){
-            maj_idx = i;
-            count = 1;
-        }
-    }
-    return num[maj_idx];
+int majorityElement(vector<int> &num) {
+    if(num.size()==0) return 0;
+    int counts = 0;
+    int candidate;
+    for_each(num.begin(), num.end(), [&counts, &candidate](int x){
+            if(counts==0) {
+                candidate = x;
+                counts = 1;
+            }else{
+                if(x == candidate) counts++;
+                else counts--;
+            }
+        });
+    return candidate;
 }
 
-TEST(FindMajority, TestCase){
-    int A[] = {2, 2, 3, 5, 2, 2, 6};
-    vector<int> num(A, A+sizeof(A)/sizeof(*A));
+TEST(FindMajority, I){
+    vector<int> num{2};
+    ASSERT_EQ(2, majorityElement(num));
+}
+
+TEST(FindMajority, II){
+    vector<int> num{2, 2, 3, 5, 2, 2, 6};
     ASSERT_EQ(2, majorityElement(num));
 }
 
