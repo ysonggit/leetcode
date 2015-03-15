516e568866d3d65e27a85f6e97320a38159fc20e
yang <ysong.sc@gmail.com>
2015-03-15 00:04:16 -0400

    AC solution excel sheet column title 1ms

--- excel_sheet_column_title.cpp
+++ excel_sheet_column_title.cpp
@@ -1,7 +1,36 @@
 #include "utils.h"
 
 using namespace std;
+/*
+  iterative version
+  given a number n = 52 should output AZ
+  n % 26 = 0 --> get 'Z'
+  n /= 26  -> n = 2, since reminder is 0, n should minus 1
+  n-- --> get 'A'
+*/
+string convertToTitle(int n) {
+    string title;
+    if(n<=0) return title;
+    while(n>0){
+        int reminder = n % 26;
+        n /= 26;
+        if(reminder == 0){
+            // append Z
+            title.append("Z");
+            n--;
+        }else{
+            char c = (char) (reminder + (int) 'A' - 1); 
+            title += string(1,c);
+        }
+    }
+    reverse(title.begin(), title.end());
+    return title;
+}
 
+/*
+  recursive version
+ */
+/*
 string toChar(int n){
     // n is in[0, 25]
     string alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
@@ -19,7 +48,7 @@ string convertToTitle(int n) {
         result = convertToTitle(rest) + toChar(remainder);
     }
     return result;
-}
+    }*/
 
 int main(int argc, char *argv[]){
     for(int i=1; i<=53; i++){
