291f8de3ef785649a5cb4d6b3b38a782766cda6a
ysonggit <ysong.sc@gmail.com>
2015-04-13 18:26:17 -0400

    binary tree bottom view

--- binary_tree_bottom_view.cpp
+++ binary_tree_bottom_view.cpp
@@ -2,30 +2,30 @@
 #include "utils.h"
 using namespace std;
 /*
-  for the nodes with the same horizontal distance, print the top one
+  Given a Binary Tree, we need to print the bottom view from left to right.
+  A node x is there in output if x is the bottommost node at its horizontal distance.
+  Horizontal distance of left child of a node x is equal to horizontal distance of x minus 1,
+  and that of right child is horizontal distance of x plus 1.
  */
-// can use pair instead also
-// typedef pair<int, TreeNode*> NodeHD;
+
 struct NodeHD{
     TreeNode * node;
     int hr_dist;
-    NodeHD(TreeNode * n, int hd) {
+    NodeHD(TreeNode *n, int hd){
         node = n;
         hr_dist = hd;
     }
 };
 
-void topView(TreeNode * root){
+void bottomView(TreeNode * root){
     if(root==NULL) return;
+    map<int, TreeNode*> hd_node;
     queue<NodeHD*> Q;
-    map<int, TreeNode*> hd_node;// map sorts its elements by keys
     Q.push(new NodeHD(root, 0));
     while(!Q.empty()){
         NodeHD * cur = Q.front();
         Q.pop();
-        if(hd_node.find(cur->hr_dist)==hd_node.end() ){
-            hd_node[cur->hr_dist] = cur->node;
-        }
+        hd_node[cur->hr_dist] = cur->node;
         if(cur->node->left != NULL){
             Q.push(new NodeHD(cur->node->left, cur->hr_dist-1));
         }
@@ -33,13 +33,13 @@ void topView(TreeNode * root){
             Q.push(new NodeHD(cur->node->right, cur->hr_dist+1));
         }
     }
-    for(auto it = hd_node.begin(); it !=hd_node.end(); ++it){
+    for(auto it = hd_node.begin(); it!= hd_node.end(); ++it){
         cout<<it->second->val<<" ";
     }
     cout<<endl;
 }
 
-TEST(TopView, I){
+TEST(BottomView, I){
     /*      1
            /  \
           2    3
@@ -54,12 +54,12 @@ TEST(TopView, I){
     vector<TreeNode*> nodes;
     treeInitializer(nodes, vals);
     printTree(nodes[0], 2);
-    cout<<"desired top view : 2 1 3 6\n";
-    cout<<"actual top view : ";
-    topView(nodes[0]);
+    cout<<"desired bottom view : 2 4 5 6\n";
+    cout<<"actual bottom view : ";
+    bottomView(nodes[0]);
 }
 
-TEST(TopView, II){
+TEST(BottomView, II){
     /*      1
            /  \
           2    3
@@ -74,14 +74,12 @@ TEST(TopView, II){
     vector<TreeNode*> nodes;
     treeInitializer(nodes, vals);
     printTree(nodes[0], 2);
-    cout<<"desired top view : 9 2 1 3 8\n";
-    cout<<"actual top view : ";
-    topView(nodes[0]);
+    cout<<"desired bottom view : 9 2 7 0 6\n";
+    cout<<"actual bottom view : ";
+    bottomView(nodes[0]);
 }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
-
