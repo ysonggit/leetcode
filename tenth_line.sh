1b807bd2a266037a7ba8e9212cb6fdf230b33078
ysonggit <ysonggit@users.noreply.github.com>
2015-05-01 21:31:47 -0400

    Create tenth_line.sh

---
+++ tenth_line.sh
@@ -0,0 +1,10 @@
+# Read from the file file.txt and output the tenth line to stdout.
+i=0
+while read line
+do
+    i=$(($i+1))
+    if [ $i == "10" ];
+    then
+        echo $line
+    fi  
+done < file.txt
