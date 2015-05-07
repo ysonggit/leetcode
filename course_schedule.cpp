a919ff8433ae0517de1603e01b2d4428833a9c1f
ysonggit <ysong.sc@gmail.com>
2015-05-07 15:09:59 -0400

    dfs topo sort for course schedule 261ms

--- course_schedule.cpp
+++ course_schedule.cpp
@@ -18,7 +18,7 @@ bool dfs(const vector<vector<int> > & pre_courses, vector<int> & visited, vector
     marked[course]=1;
     return true;
 }
-
+/*
 bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
     if(prerequisites.size()==0) return true;
     vector< vector<int> >pre_courses(numCourses, vector<int>(0));
@@ -33,7 +33,42 @@ bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         if(!dfs(pre_courses, visited, marked, i)) return false;
     }
     return true;
+    }*/
+
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    if(prerequisites.size()==0) return true;
+    vector< vector<int> >pre_courses(numCourses, vector<int>(0));
+    // trick use an array to save the indegree of a node
+    // indegree[i] = j means for node i, indegree is j
+    vector<int> indegree(numCourses, 0);
+    for(auto coursepair : prerequisites){
+        int course = coursepair[0], pre = coursepair[1];
+        pre_courses[pre].push_back(course);
+        indegree[course]++;
+    }
+    queue<int> courses_without_pre;
+    for(int i=0; i<numCourses; i++){
+        // find nodes without indegree
+        if(indegree[i]==0) courses_without_pre.push(i);
+    }
+    while(!courses_without_pre.empty()){
+        int n = courses_without_pre.front();
+        courses_without_pre.pop();
+        // for each m with edge e from n to m
+        // remove e from graph and if for m no in edges, push to queue
+        for(int m : pre_courses[n]){
+            indegree[m]--;
+            if(indegree[m]==0){
+                courses_without_pre.push(m);
+            }
+        }
+    }
+    for(int i=0; i<numCourses; i++){
+        if(indegree[i] >0) return false;
+    }
+    return true;
 }
+
 
 TEST(COURSE, I){
     vector<vector<int>> prerequisites{
