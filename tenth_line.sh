909be041a71b386227d61a8711f090e784a0d809
ysonggit <ysonggit@users.noreply.github.com>
2015-05-08 14:48:03 -0400

    Update tenth_line.sh

--- tenth_line.sh
+++ tenth_line.sh
@@ -1,10 +1,11 @@
 # Read from the file file.txt and output the tenth line to stdout.
-i=0
-while read line
-do
-    i=$(($i+1))
-    if [ $i == "10" ];
-    then
-        echo $line
-    fi  
-done < file.txt
+#i=0
+#while read line
+#do
+#    i=$(($i+1))
+#    if [ $i == "10" ];
+#    then
+#        echo $line
+#    fi  
+#done < file.txt
+sed '10q;d' file.txt
